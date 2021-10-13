ac2935c01be7a6383120e5116e7c17399378843a
Sarahmaliin <sarahmalinkarlsson@gmail.com>
2021-10-13 12:14:09 +0200

    notification works

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,20 @@ import React, { useState } from 'react';
 import './App.css';
 import Clock from './Components/Clock';
 import HelloWorld from './Components/HelloWorld';
+import Notification from './Components/Notification';
 
 function App() {
 
   const [showClock, setShowClock] = useState<boolean>(true)
-
+  const [message, setMessage] = useState<string>('Notification')
 
   return (
     <div className="App">
       <HelloWorld />
       <button onClick={() => setShowClock(!showClock)}> Toggla klockan </button>
             { showClock ? <Clock /> : null}
+
+      <Notification message={message} />
     </div>
   );
 }
---
+++ src/Components/Notification.tsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react"
+
+interface NotificationProps {
+    message: string;
+}
+
+const Notification = ({message}: NotificationProps) =>{
+
+    const [seconds, setSeconds] = useState<number>(0)
+    const [noteVisible, setNoteVisible] = useState<string>('hidden')
+
+    useEffect(() =>{
+        let intervalTime = setTimeout(() =>{
+            setSeconds(s => s + 1)
+            console.log(seconds, 'timeout')
+            setNoteVisible('show')
+        }, 3000)
+
+        return () =>clearTimeout(intervalTime)
+    }, [seconds])
+
+
+    return(
+        <section className={noteVisible}>
+        <h1 >{message}</h1>
+        </section>
+    )
+}
+
+export default Notification
