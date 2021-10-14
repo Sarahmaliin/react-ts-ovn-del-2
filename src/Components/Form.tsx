dc30c30bde2aa6b34ebe426f91d489cad3cec78d
Sarahmaliin <sarahmalinkarlsson@gmail.com>
2021-10-14 10:22:48 +0200

    update form

--- src/Components/Form.tsx
+++ src/Components/Form.tsx
@@ -2,23 +2,17 @@ import { useState } from "react"
 
 const Form = () =>{
 
-    const [ name, setName ] = useState<string>('')
-    const [ lastName, setLastName ] = useState<string>('')
+    const [ name, setName ] = useState<string>('förnamn')
+    const [ lastName, setLastName ] = useState<string>('efternamn')
 
     const handleNameChange = (event: any) =>{
         event.preventDefault()
         setName(event.target.value.toUpperCase())
     }
 
-    const handleLastNameChange = (event: any) =>{
-        event.preventDefault()
-        setLastName(event.target.value.toUpperCase())
-    }
-
     return(
         <section className="component">
             <input type="text" onChange={handleNameChange} value={name}/>
-            <input type="text" onChange={handleLastNameChange} value={lastName} />
         </section>
     )
 }
